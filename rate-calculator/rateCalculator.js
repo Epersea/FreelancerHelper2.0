9f5a5b075bcd31020050180281afbc3b9b733a2f
elisafvic <elisapersea@gmail.com>
2023-01-24 17:44:08 +0100

    Annual expenses calculation completed.

--- rate-calculator/rateCalculator.js
+++ rate-calculator/rateCalculator.js
@@ -1,18 +1,41 @@
 class RateCalculator {
 
     calculateAnnualExpenses(expenses) {
-        let annualExpenses = 0;
-        annualExpenses += this.calculateLongTermExpenses(expenses)
+        let annualExpenses = 0; 
+        annualExpenses += this.calculateLongTermExpenses(expenses);
+        annualExpenses += this.calculateYearlyExpenses(expenses);
+        annualExpenses += this.calculateMonthlyExpenses(expenses);
 
         return annualExpenses;
     }
     
     calculateLongTermExpenses(expenses) {
         let longTermExpenses = 0;
-        for (let expense of expenses["long-term"]) {
-            longTermExpenses += (expense.price / expense.years);
+        if (expenses["long-term"]) {
+            for (let expense of expenses["long-term"]) {
+                longTermExpenses += (expense.price / expense.years);
+            }
         }
+
         return longTermExpenses;
+    }
+
+    calculateYearlyExpenses(expenses) {
+        let yearlyExpenses = 0;
+        if (expenses.yearly) {
+            yearlyExpenses += expenses.yearly;
+        }
+
+        return yearlyExpenses;
+    }
+
+    calculateMonthlyExpenses(expenses) {
+        let monthlyExpenses = 0;
+        if (expenses.monthly) {
+            return expenses.monthly * 12;
+        } 
+        
+        return monthlyExpenses;
     }
 }
 
--- rate-calculator/test.js
+++ rate-calculator/test.js
@@ -1,27 +1,58 @@
 const assert = require('assert');
-//const { describe } = require('mocha');
-//const { it } = require('mocha');
 const RateCalculator = require('./rateCalculator.js');
 
 const calculator = new RateCalculator;
 
 describe('Rate Calculator', () => {
 
-    it('Calculates annual impact of long-term expenses', () => {
-        const expenses = {
-            "long-term" : [
-                {
-                    price: 1500,
-                    years: 5
-                },
-                {
-                    price: 1000,
-                    years: 10
-                }]
-        }
-        
-        const annualExpenses = calculator.calculateAnnualExpenses(expenses);
-        
-        assert.equal(annualExpenses, 400)
+    describe('Expenses', () => {
+
+        it('Calculates annual impact of long-term expenses', () => {
+            const expenses = {
+                "long-term": [
+                    {
+                        price: 1500,
+                        years: 5
+                    },
+                    {
+                        price: 1000,
+                        years: 10
+                    }]
+            }
+
+            const annualExpenses = calculator.calculateAnnualExpenses(expenses);
+
+            assert.equal(annualExpenses, 400)
+        })
+
+        it('Calculates annual impact of monthly expenses', () => {
+            const expenses = {
+                monthly: 100,
+            }
+
+            const annualExpenses = calculator.calculateAnnualExpenses(expenses);
+
+            assert.equal(annualExpenses, 1200)
+        })
+
+        it('Aggregates long-term, annual and monthly expenses', () => {
+            const expenses = {
+                "long-term": [
+                    {
+                        price: 1500,
+                        years: 5
+                    },
+                    {
+                        price: 1000,
+                        years: 10
+                    }],
+                yearly: 1000,
+                monthly: 100
+            }
+
+            const annualExpenses = calculator.calculateAnnualExpenses(expenses);
+
+            assert.equal(annualExpenses, 2600)
+        })
     })
 })
